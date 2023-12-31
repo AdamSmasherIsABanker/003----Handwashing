28df216ac1962ad43a02d2233f8572b0fe3f574e
Yuvan Boaz Ramesh <adamsmasherbank@gmail.com>
2024-01-01 00:13:44 +0530

    added clinic with most deaths reported

---
+++ analyse.py
@@ -0,0 +1,20 @@
+# import the pandas api to analyse data
+import pandas as dt
+
+# setup dataframe
+data = dt.read_csv('yearly_deaths_by_clinic.csv')
+
+# total deaths
+totalDeaths = data.max()['deaths']
+# beggining year
+minYear = data.min()['year']  
+# ending year
+maxYear = data.max()['year']  
+
+# most death reported clinic
+mostDeath = data['clinic'].describe()['top']
+
+# basic info
+print("handwashing has contributed to "+str(totalDeaths)+" deaths from the year "+str(minYear)+" to the year "+str(maxYear)+".")
+print("")
+print("Most deaths were reported by " + mostDeath)
